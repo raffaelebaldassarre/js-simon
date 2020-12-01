901a51d4d89784df31f2667dcbdc5afef1ea68bd
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2020-12-01 23:52:21 +0100

    Finito

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,14 @@ $(function() {
     var game = $(".game")
     var numeriCasuali=[];
     var numeriAnswer=[];
+    var numeriRicordati=[];
+    var numeriNonRicordati=[];
 
-    for (var i = 0; i < 5; i++) {
+    while (numeriCasuali.length < 5) {
         var randomNumber = Math.floor((Math.random() * 100) + 1);
-    numeriCasuali.push(randomNumber);
+        if(!(numeriCasuali.includes(randomNumber))){
+           numeriCasuali.push(randomNumber);
+        }
     }
     console.log(numeriCasuali);
     //Un alert espone 5 numeri casuali diversi.
@@ -13,17 +17,43 @@ $(function() {
     
     setTimeout(function () {
         game.addClass("remove");//Faccio scomparire i numeri generati dal pc
-    }, 4500);
+    }, 29500);
 
     //Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
     setTimeout(function(){
-        for (var i = 0; i < 5; i++) {
-            var numberQuestion = Number(prompt("Scrivi uno alla volta i numeri che ricordi"));
-            numeriAnswer.push(numberQuestion);
+
+        while (numeriAnswer.length < 5) {
+            var numberUser = Number(prompt("Scrivi uno alla volta i numeri che ricordi"));
+            while (isNaN(numberUser)){
+                alert("Attenzione, questo non è un numero");
+                numberUser = Number(prompt("Dai che lo ricordi.. è il numero.."));
+            }
+            if(!(numeriAnswer.includes(numberUser)) && numberUser <= 100 && numberUser > 0){
+                numeriAnswer.push(numberUser);
+            }else {
+                alert("Hey ricorda, hai già inserito questo numero oppure non è compreso tra 1 e 100, prova con un altro numero");
+            }
         }
         console.log(numeriAnswer);
-    },5000);
+        // //Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati. */
+        for (var j = 0; j < numeriAnswer.length; j++) {
+            if(numeriCasuali.includes(numeriAnswer[j])){
+                numeriRicordati.push(numeriAnswer[j]);
+            }else{
+                numeriNonRicordati.push(numeriCasuali[j]);
+            }
+        }
+        console.log(numeriRicordati);
+        console.log(numeriNonRicordati);
 
-    //Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati. */
+        if(numeriRicordati.length==0){
+            alert("Non hai ricordato nessun numero, dai non disperare fra poco avrai dimenticato tutto");
+        }else if (numeriNonRicordati.length==0){
+            alert("Bravissimo hai ricordato tutti i numeri");
+        }else /* if(numeriCasuali != numeriRicordati[j]) */{
+            alert("Hai ricordato " + numeriRicordati.length + " numeri " + numeriRicordati);
+            alert("Però non hai ricordato " + numeriNonRicordati.length + " numeri " + numeriNonRicordati);
+        }
+    },30000);
 
 });
